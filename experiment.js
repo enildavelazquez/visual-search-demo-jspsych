8f8c90f0b61fd1fb9210265e3861ded2feadf508
ev <enildavelazquez@outlook.com>
2023-02-20 11:05:29 -0500

    timeline variable stopped working

--- experiment.js
+++ experiment.js
@@ -35,8 +35,69 @@ var instructions = {
 };
 timeline.push(instructions);
 
+// test trials - long form (timeline function issues)
+var trial_1 = {
+    type: jsPsychVisualSearchCircle,
+    stimuli: [`img/c.png`, `img/h.png`, `img/e.png`, `img/l.png`, `img/p.png`, `img/s.png`],
+    fixation_image: `img/fixation.png`,
+    fixation_size: [35, 35],
+    target_present_key: 'f',
+    target_absent_key: 'j',
+    target_present: true,
+    post_trial_gap: 1000,
+};
+timeline.push(trial_1);
+
+var trial_2 = {
+    type: jsPsychVisualSearchCircle,
+    stimuli: [`img/c.png`, `img/l.png`, `img/e.png`, `img/u.png`, `img/s.png`, `img/p.png`],
+    fixation_image: `img/fixation.png`,
+    fixation_size: [35, 35],
+    target_present_key: 'j',
+    target_absent_key: 'f',
+    target_present: true,
+    post_trial_gap: 1000,
+};
+timeline.push(trial_2);
+
+var trial_3 = {
+    type: jsPsychVisualSearchCircle,
+    stimuli: [`img/c.png`, `img/p.png`, `img/l.png`, `img/u.png`, `img/s.png`, `img/e.png`],
+    fixation_image: `img/fixation.png`,
+    fixation_size: [35, 35],
+    target_present_key: 'j',
+    target_absent_key: 'f',
+    target_present: true,
+    post_trial_gap: 1000,
+};
+timeline.push(trial_3);
+
+var trial_4 = {
+    type: jsPsychVisualSearchCircle,
+    stimuli: [`img/s.png`, `img/e.png`, `img/l.png`, `img/c.png`, `img/h.png`, `img/p.png`],
+    fixation_image: `img/fixation.png`,
+    fixation_size: [35, 35],
+    target_present_key: 'f',
+    target_absent_key: 'j',
+    target_present: true,
+    post_trial_gap: 1000,
+};
+timeline.push(trial_4);
+
+var trial_5 = {
+    type: jsPsychVisualSearchCircle,
+    stimuli: [`img/p.png`, `img/s.png`, `img/e.png`, `img/c.png`, `img/l.png`, `img/h.png`],
+    fixation_image: `img/fixation.png`,
+    fixation_size: [35, 35],
+    target_present_key: 'f',
+    target_absent_key: 'j',
+    target_present: true,
+    post_trial_gap: 1000,
+};
+timeline.push(trial_5);
+
 //timeline variable function that isn't working :)
-var test_stimuli = [
+/* var test_stimuli = [
     { stimulus: 'img/c.png' },
     { stimulus: 'img/e.png' },
     { stimulus: 'img/p.png' },
@@ -60,7 +121,7 @@ var trial_procedure = {
     randomize_order: true,
     repetitions: 5
 };
-timeline.push(trial_procedure);
+timeline.push(trial_procedure); */
 
 // debrief 
 var debrief = {
